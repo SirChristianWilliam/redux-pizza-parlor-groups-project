96895ae2ca6d0b27314824a583f5b90718865e34
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-27 14:05:59 -0500

    need to add remove pizza from cart

--- src/index.js
+++ src/index.js
@@ -7,13 +7,51 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
+const cartPizzas = (state = [], action) => {
+    // TODO: Save Products added to the cart
+    switch (action.type) {
+        case 'ADD_PIZZA_TO_CART':
+            return [...state, action.payload];
+        case 'REMOVE_PIZZA_FROM_CART':
+            // TODO, need to remove
+            logger('NEED TO REMOVE ITEM FROM PIZZA CART');
+        case 'CLEAR_PIZZACART_AND_CUSTOMERINFO':
+            return [];
+    }
+
+    return state;
+};
+
+const customerInfo = (state = {}, action) => {
+
+    switch (action.type) {
+        case 'ADD_CUSTOMERINFO':
+            return action.payload;
+        case 'CLEAR_PIZZACART_AND_CUSTOMERINFO':
+            return {};
+        
+    }
+};
+
+const pizzas = (state = [], action) => {
+    // TODO: Save all products from the server
+    switch (action.type) {
+        case 'SET_PIZZAS':
+            return action.payload;
+    }
+    return state;
+};
+
 // The store is the big JavaScript Object that holds all of the information for our application
 const storeInstance = createStore(
     combineReducers({
-
+        pizzas,
+        cartPizzas,
+        customerInfo
     }),
     applyMiddleware(logger)
 );
+
 
 ReactDOM.render(
     <Provider store={storeInstance} >
