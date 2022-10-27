2e91fd6bf2ef1c6ce1152ab6bcff1a9b5c0e073d
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-27 10:22:14 -0500

    skeleton index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    <Provider store={storeInstance} >
+        <App />
+    </Provider>
+    , document.getElementById('root'));
