8ef7628020c5e7839c16eaf50248aff204ef4390
Chris Maki <c.william.developer@gmail.com>
2022-10-28 10:56:56 -0500

    Got pretty far

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,61 +1,63 @@
-import {useState} from "react";
-import {useSelector} from 'react-redux'
+import { useState } from "react";
+import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux';
 import './PizzaItem.css';
 
-function PizzaItem({pizza, cart}) {
-  const [removeBtn, setRemoveBtn] = useState(false)
-  const dispatch = useDispatch();
-  const cartPizzas = useSelector(store => store.cartPizzas);
-
-  
+function PizzaItem({ pizza, cart }) {
+    const [removeBtn, setRemoveBtn] = useState(false)
+    const dispatch = useDispatch();
+    const cartPizzas = useSelector(store => store.cartPizzas);
 
 
-  const getRemove = () => {
-    setRemoveBtn(!removeBtn)
-    dispatch({
-      type:'REMOVE_PIZZA_FROM_CART',
-      payload: {id: pizza.id,
-        name: pizza.name, 
-        price: Number(pizza.price)
-      }
-      })
 
-    
-  
-  }
+
+    const getRemove = () => {
+        setRemoveBtn(!removeBtn)
+        dispatch({
+            type: 'REMOVE_PIZZA_FROM_CART',
+            payload: {
+                id: pizza.id,
+                name: pizza.name,
+                price: Number(pizza.price)
+            }
+        })
+
+
+
+    }
 
 
     const addCart = () => {
         console.log(pizza);
         // TODO: Dispatch here
         dispatch({
-          type:'ADD_PIZZA_TO_CART',
-          payload: {id: pizza.id,
-            name: pizza.name, 
-            price: Number(pizza.price)
-          }
+            type: 'ADD_PIZZA_TO_CART',
+            payload: {
+                id: pizza.id,
+                name: pizza.name,
+                price: Number(pizza.price)
+            }
 
-      });
-      }
-    
+        });
+    }
+
     return (
-        <div className = "items">
-        <>
-        
-            <h2> {pizza.name}</h2>
-            <p>{pizza.description}</p>
-            <h4>${pizza.price}</h4>
+        <div className="items">
+            <>
+
+                <h2> {pizza.name}</h2>
+                <p>{pizza.description}</p>
+                <h4>${pizza.price}</h4>
             </>
 
-     
+
 
             {cartPizzas.map((cart, i) => {
-          return <button key = {i} cart ={cart} onClick ={getRemove}>Remove</button> ;
-          })}
-            <button onClick ={addCart}>Add</button>
+                return <button key={i} cart={cart} onClick={getRemove}>Remove</button>;
+            })}
+            <button onClick={addCart}>Add</button>
 
-            
+
 
         </div>
 
