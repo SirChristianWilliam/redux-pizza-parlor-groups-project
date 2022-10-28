599b8bf2cb4b9e9739c11197137ff57d1adef9ff
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-28 09:38:39 -0500

    added admin page

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom'
-
+import './AdminPage.css'
 function AdminPage() {
 
+    const [orders,setOrders] = useState([]);
+
     const params = useParams();
+
     useEffect(()=>{
         axios({
             method: 'GET',
             url: '/api/order'
         })
         .then((response)=>{
-
+            setOrders(response.data);
         })
         .catch((error)=>{
             console.error('Admin /api/order GET error',error);
@@ -21,9 +24,28 @@ function AdminPage() {
 
 
     return (
-        <>
-            <h2>AdminPage Component</h2>
-        </>
+        <div className='tableDiv'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Time Order Placed</th>
+                        <th>Type</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order)=>(
+                        <tr key={order.id}>
+                            <td>{order.customer_name}</td>
+                            <td>{order.time}</td>
+                            <td>{order.type}</td>
+                            <td>{order.total}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
